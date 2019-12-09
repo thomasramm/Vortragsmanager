92f4d05b15c0824f5a6c32a130c1f38ce7a3dad0
Thomas Ramm <post@thomas-ramm.de>
2019-12-09 23:01:11 +0100

    Rohdaten zusammengefasst

--- Vortragsmanager/MainWindow.xaml.cs
+++ Vortragsmanager/MainWindow.xaml.cs
@@ -10,7 +10,7 @@ namespace Vortragsmanager
     {
         public MainWindow()
         {
-            Core.DataContainer.ReadData(@"C:\Daten\Thomas\Projekte\Vortragsmanager\Data.xlsx");
+            Core.DataContainer.ReadData(@"C:\Daten\Thomas\Projekte\Vortragsmanager\Rohdaten\Data.xlsx");
             Core.Templates.LoadTemplates();
             Console.WriteLine($"{Core.DataContainer.Vorträge.Count} Vorträge, {Core.DataContainer.Versammlungen.Count} Versammlungen, {Core.DataContainer.Redner.Count} Redner geladen");
             InitializeComponent();
