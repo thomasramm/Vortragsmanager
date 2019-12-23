2855e728f3624ef44b0bc2b02b97df4f6a907e24
Thomas Ramm <post@thomas-ramm.de>
2019-12-24 00:58:29 +0100

    Backupdatei bei jedem Speichern anlegen

--- Vortragsmanager/App.xaml.cs
+++ Vortragsmanager/App.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows;
+﻿using System;
+using System.IO;
+using System.Windows;
 using Vortragsmanager.Core;
 using Vortragsmanager.Properties;
 
@@ -12,7 +14,14 @@ namespace Vortragsmanager
         private void Application_Exit(object sender, ExitEventArgs e)
         {
             if (DataContainer.IsInitialized)
-                IoSqlite.SaveContainer(Settings.Default.sqlite);
+            {
+                var file = IoSqlite.SaveContainer(Settings.Default.sqlite);
+
+                //Aktuell wird jedes mal eine Sicherheitskopie erstellt
+                var fi = new FileInfo(file);
+                var backup = fi.DirectoryName + "\\" + fi.Name.Substring(0, fi.Name.Length - fi.Extension.Length) + $"_{DateTime.Today:yyyy-MM-dd-hh-mm}" + fi.Extension;
+                File.Copy(file, backup, true);
+            }
         }
     }
 }
