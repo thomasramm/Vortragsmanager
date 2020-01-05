84f41d9ef501fc0b71830e25a2189c9151216d1c
Thomas Ramm <post@thomas-ramm.de>
2020-01-05 20:45:39 +0100

    neue Listen hinzugefügt

--- Vortragsmanager/Views/ListCreateViewModel.cs
+++ Vortragsmanager/Views/ListCreateViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using DevExpress.Mvvm;
 using OfficeOpenXml;
+using OfficeOpenXml.Table;
 using System;
 using System.IO;
 using System.Linq;
@@ -18,12 +19,18 @@ namespace Vortragsmanager.Views
         {
             CreateAushangCommand = new DelegateCommand(CreateAushang);
             CreateContactListCommand = new DelegateCommand(CreateContactList);
+            CreateExchangeRednerListCommand = new DelegateCommand(CreateExchangeRednerList);
+            CreateOverviewTalkCountCommand = new DelegateCommand(CreateOverviewTalkCount);
             templateFolder = AppDomain.CurrentDomain.BaseDirectory + @"Templates\";
         }
 
         public DelegateCommand CreateAushangCommand { get; private set; }
 
         public DelegateCommand CreateContactListCommand { get; private set; }
+
+        public DelegateCommand CreateExchangeRednerListCommand { get; private set; }
+
+        public DelegateCommand CreateOverviewTalkCountCommand { get; private set; }
 
         private static string GetRednerAuswärts(DateTime datum)
         {
@@ -40,8 +47,7 @@ namespace Vortragsmanager.Views
             return ausgabe.Substring(0, ausgabe.Length - 2);
         }
 
-        [System.Diagnostics.CodeAnalysis.SuppressMessage("Performance", "CA1822:Mark members as static", Justification = "<Pending>")]
-        public void CreateAushang()
+        public static void CreateAushang()
         {
             //laden der Excel-Datei
             var template = $"{templateFolder}AushangExcel.xlsx";
@@ -94,8 +100,7 @@ namespace Vortragsmanager.Views
             SaveExcelFile(tempFile, "Aushang.xlsx");
         }
 
-        [System.Diagnostics.CodeAnalysis.SuppressMessage("Performance", "CA1822:Mark members as static", Justification = "<Pending>")]
-        public void CreateContactList()
+        public static void CreateContactList()
         {
             var tempFile = Path.GetTempFileName();
             var excel = new FileInfo(tempFile);
@@ -173,6 +178,173 @@ namespace Vortragsmanager.Views
             SaveExcelFile(tempFile, "Kontaktdaten.xlsx");
         }
 
+        public static void CreateExchangeRednerList()
+        {
+            var jahr = DateTime.Today.Year;
+            var row = 12;
+            var tempFile = Path.GetTempFileName();
+            var excel = new FileInfo(tempFile);
+            using (ExcelPackage package = new ExcelPackage())
+            {
+                ExcelWorksheet sheet = package.Workbook.Worksheets.Add($"Redner");
+
+                sheet.Column(1).Width = 30;
+                sheet.Column(2).Width = 25;
+                sheet.Column(3).Width = 30;
+
+                sheet.Cells[1, 1, 1, 3].Style.Font.Bold = true;
+                sheet.Cells[1, 1].Value = "Versammlungsdaten";
+                sheet.Cells[2, 1].Value = $"{DataContainer.MeineVersammlung.Name} ({DataContainer.MeineVersammlung.Kreis})";
+                row = 3;
+                if (!string.IsNullOrEmpty(DataContainer.MeineVersammlung.Anschrift1))
+                {
+                    sheet.Cells[row, 1].Value = DataContainer.MeineVersammlung.Anschrift1;
+                    row++;
+                }
+                if (!string.IsNullOrEmpty(DataContainer.MeineVersammlung.Anschrift2))
+                {
+                    sheet.Cells[row, 1].Value = DataContainer.MeineVersammlung.Anschrift2;
+                    row++;
+                }
+                if (!string.IsNullOrEmpty(DataContainer.MeineVersammlung.Anreise))
+                {
+                    sheet.Cells[row, 1].Value = DataContainer.MeineVersammlung.Anreise;
+                    row++;
+                }
+                if (!string.IsNullOrEmpty(DataContainer.MeineVersammlung.Telefon))
+                {
+                    sheet.Cells[row, 1].Value = DataContainer.MeineVersammlung.Telefon;
+                    row++;
+                }
+                var maxRow = row;
+
+                sheet.Cells[1, 3].Value = "Koordinatordaten";
+                sheet.Cells[2, 3].Value = DataContainer.MeineVersammlung.Koordinator;
+                row = 3;
+                if (!string.IsNullOrEmpty(DataContainer.MeineVersammlung.KoordinatorTelefon))
+                {
+                    sheet.Cells[row, 3].Value = DataContainer.MeineVersammlung.KoordinatorTelefon;
+                    row++;
+                }
+                if (!string.IsNullOrEmpty(DataContainer.MeineVersammlung.KoordinatorMobil))
+                {
+                    sheet.Cells[row, 3].Value = DataContainer.MeineVersammlung.KoordinatorMobil;
+                    row++;
+                }
+                if (!string.IsNullOrEmpty(DataContainer.MeineVersammlung.KoordinatorMail))
+                {
+                    sheet.Cells[row, 3].Value = DataContainer.MeineVersammlung.KoordinatorMail;
+                    row++;
+                }
+                if (!string.IsNullOrEmpty(DataContainer.MeineVersammlung.KoordinatorJw))
+                {
+                    sheet.Cells[row, 3].Value = DataContainer.MeineVersammlung.KoordinatorJw;
+                    row++;
+                }
+                row = Math.Max(row, maxRow) + 1;
+
+                sheet.Cells[row, 1].Style.Font.Bold = true;
+                sheet.Cells[row, 1].Value = "Zusammenkunftszeiten";
+                row++;
+
+                sheet.Cells[row, 1].Value = $"{jahr}: {DataContainer.MeineVersammlung.GetZusammenkunftszeit(jahr)}";
+                row++;
+
+                sheet.Cells[row, 1].Value = $"{jahr + 1}: {DataContainer.MeineVersammlung.GetZusammenkunftszeit(jahr + 1)}";
+                row = row + 2;
+
+                sheet.Cells[row, 1, row, 3].Style.Font.Bold = true;
+                sheet.Cells[row, 1].Value = "Redner";
+                sheet.Cells[row, 2].Value = "Telefonnummer";
+                sheet.Cells[row, 3].Value = "Vorträge";
+                row++;
+
+                var anzahlDag = 0;
+                var anzahlEingeschränkt = 0;
+
+                foreach (var redner in DataContainer.Redner.Where(x => x.Versammlung == DataContainer.MeineVersammlung && x.Aktiv))
+                {
+                    sheet.Cells[$"A{row}:C{row}"].Style.WrapText = true;
+
+                    var name = redner.Name;
+                    if (!redner.Ältester)
+                    {
+                        name += " #";
+                        anzahlDag += 1;
+                    }
+
+                    sheet.Cells[row, 1].Value = name;
+
+                    sheet.Cells[row, 2].Value = redner.Telefon ?? redner.Mobil;
+                    var vorträge = string.Empty;
+                    foreach (var v in redner.Vorträge)
+                    {
+                        vorträge += $"{v.Nummer}, ";
+                    }
+                    sheet.Cells[row, 3].Value = vorträge.TrimEnd().TrimEnd(',');
+                    row++;
+                }
+
+                row++;
+                if (anzahlDag > 0)
+                {
+                    sheet.Cells[row, 1].Value = "# Dienstamtgehilfe";
+                    row++;
+                }
+                if (anzahlEingeschränkt > 0)
+                {
+                    sheet.Cells[row, 1].Value = "* Eingeschränkter Reiseradius";
+                }
+
+                sheet.Cells[row, 1].Value = $"Stand: {DateTime.Today:dd.MMMM.yyyy}";
+
+                package.SaveAs(excel);
+            }
+            SaveExcelFile(tempFile, "Rednerliste.xlsx");
+        }
+
+        public static void CreateOverviewTalkCount()
+        {
+            var tempFile = Path.GetTempFileName();
+            var excel = new FileInfo(tempFile);
+            var vers = DataContainer.MeineVersammlung;
+            var kreis = vers.Kreis;
+            using (ExcelPackage package = new ExcelPackage())
+            {
+                ExcelWorksheet sheet = package.Workbook.Worksheets.Add($"Vorträge");
+
+                sheet.Column(1).Width = 10;
+                sheet.Column(2).Width = 50;
+                sheet.Column(3).Width = 10;
+
+                sheet.Cells[1, 1].Value = "Anzahl der Ausarbeitungen der Vorträge";
+
+                sheet.Cells[2, 1, 2, 4].Style.Font.Bold = true;
+                sheet.Cells[2, 1].Value = "Nr";
+                sheet.Cells[2, 2].Value = "Thema";
+                sheet.Cells[2, 3].Value = "Versammlung";
+                sheet.Cells[2, 4].Value = "Kreis";
+
+                var row = 3;
+                foreach (var v in DataContainer.Vorträge.OrderBy(x => x.Nummer))
+                {
+                    sheet.Cells[row, 1].Value = v.Nummer;
+                    sheet.Cells[row, 2].Value = v.Thema;
+                    sheet.Cells[row, 3].Value = DataContainer.Redner.Where(x => x.Versammlung == vers && x.Vorträge.Contains(v)).Count();
+                    sheet.Cells[row, 4].Value = DataContainer.Redner.Where(x => x.Versammlung.Kreis == kreis && x.Vorträge.Contains(v)).Count();
+                    row++;
+                }
+
+                //create a range for the table
+                ExcelRange range = sheet.Cells[2, 1, row - 1, 4];
+                ExcelTable tab = sheet.Tables.Add(range, "Table1");
+                tab.TableStyle = TableStyles.Medium2;
+
+                package.SaveAs(excel);
+            }
+            SaveExcelFile(tempFile, "Vortragsthemen.xlsx");
+        }
+
         private static void SaveExcelFile(string tempName, string sugestedName)
         {
             var saveFileDialog1 = new SaveFileDialog
@@ -185,8 +357,25 @@ namespace Vortragsmanager.Views
 
             if (saveFileDialog1.ShowDialog() == DialogResult.OK)
             {
-                File.Delete(saveFileDialog1.FileName);
-                File.Move(tempName, saveFileDialog1.FileName);
+                var fi = new FileInfo(saveFileDialog1.FileName);
+                var filename = fi.FullName;
+                var i = 0;
+                try
+                {
+                    File.Delete(filename);
+                }
+                catch
+                {
+                    while (File.Exists(filename))
+                    {
+                        i++;
+                        filename = $"{fi.DirectoryName}\\{fi.Name.Substring(0, fi.Name.Length - fi.Extension.Length)} ({i}){fi.Extension}";
+                    }
+                }
+                finally
+                {
+                    File.Move(tempName, filename);
+                }
             }
             saveFileDialog1.Dispose();
         }
