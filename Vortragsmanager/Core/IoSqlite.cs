0c1784cc9b7d86fe27e15aa68598d8cbacc6001c
Thomas Ramm <post@thomas-ramm.de>
2020-01-18 11:56:38 +0100

    Bug Datenbankversion wurde nicht gespeichert

--- Vortragsmanager/Core/IoSqlite.cs
+++ Vortragsmanager/Core/IoSqlite.cs
@@ -873,7 +873,7 @@ namespace Vortragsmanager.Core
             cmd.Parameters.Add("@Wert", System.Data.DbType.String);
 
             cmd.Parameters[0].Value = "Version";
-            cmd.Parameters[1].Value = 1;
+            cmd.Parameters[1].Value = DataContainer.Version;
             cmd.ExecuteNonQuery();
 
             cmd.Parameters[0].Value = "IsInitialized";
