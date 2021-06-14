e07ea2fc5ea1a1a32c6eb5f96b4118e5765f6b1b
Thomas Ramm <thomas.ramm@gmx.de>
2021-06-14 13:19:14 +0200

    AktivitätenLog Anzeigebug behoben

--- Vortragsmanager/ActivityLog/Activities.cs
+++ Vortragsmanager/ActivityLog/Activities.cs
@@ -24,7 +24,7 @@ namespace Vortragsmanager.ActivityLog
 
         private void SetFilter()
         {
-            foreach (var a in Alle)
+            foreach (var a in alleUnfiltered)
             {
                 if (_filterAktivität == "Alle"
                     || (_filterAktivität == "Meine Redner"
@@ -63,6 +63,38 @@ namespace Vortragsmanager.ActivityLog
                 else
                     a.Aktiv = false;
             }
+
+            Heute.Clear();
+            DieseWoche.Clear();
+            DieserMonat.Clear();
+            LetzterMonat.Clear();
+            DiesesJahr.Clear();
+            Älter.Clear();
+            foreach (var item in alleUnfiltered.Where(x => x.Aktiv))
+            {
+                switch (item.Zeitraum)
+                {
+                    case ActivityTime.Heute:
+                        Heute.Add(item);
+                        break;
+                    case ActivityTime.DieseWoche:
+                        DieseWoche.Add(item);
+                        break;
+                    case ActivityTime.DieserMonat:
+                        DieserMonat.Add(item);
+                        break;
+                    case ActivityTime.LetzterMonat:
+                        LetzterMonat.Add(item);
+                        break;
+                    case ActivityTime.DiesesJahr:
+                        DiesesJahr.Add(item);
+                        break;
+                    case ActivityTime.Älter:
+                        Älter.Add(item);
+                        break;
+                }
+            }
+
             RaisePropertyChanged(nameof(HeuteHeader));
             RaisePropertyChanged(nameof(DieseWocheHeader));
             RaisePropertyChanged(nameof(DieserMonatHeader));
@@ -115,12 +147,7 @@ namespace Vortragsmanager.ActivityLog
 
         public void Initialize()
         {
-            Heute.Clear();
-            DieseWoche.Clear();
-            DieserMonat.Clear();
-            DiesesJahr.Clear();
-            Älter.Clear();
-            Alle.Clear();
+            alleUnfiltered.Clear();
 
             _nextId = DataContainer.Aktivitäten.Select(x => x.Id).DefaultIfEmpty(0).Max() + 1;
 
@@ -138,23 +165,24 @@ namespace Vortragsmanager.ActivityLog
             foreach (var a in DataContainer.Aktivitäten.OrderByDescending(x => x.Datum))
             {
                 var item = new Item(a);
-                Alle.Add(item);
+                alleUnfiltered.Add(item);
 
+                //Zeitraum festlegen
                 if (a.Datum.Date == DateTime.Today)
-                    Heute.Add(item);
+                    item.Zeitraum = ActivityTime.Heute;
                 else if (a.Datum.Year == DateTime.Today.Year)
                 {
                     if (week == CultureInfo.InvariantCulture.Calendar.GetWeekOfYear(a.Datum, CalendarWeekRule.FirstFourDayWeek, DayOfWeek.Monday))
-                        DieseWoche.Add(item);
+                        item.Zeitraum = ActivityTime.DieseWoche;
                     else if (a.Datum.Month == DateTime.Today.Month)
-                        DieserMonat.Add(item);
+                        item.Zeitraum = ActivityTime.DieserMonat;
                     else if (a.Datum.Month == ((DateTime.Today.Month == 1) ? 12 : DateTime.Today.Month - 1))
-                        LetzterMonat.Add(item);
+                        item.Zeitraum = ActivityTime.LetzterMonat;
                     else
-                        DiesesJahr.Add(item);
+                        item.Zeitraum = ActivityTime.DiesesJahr;
                 }
                 else
-                    Älter.Add(item);
+                    item.Zeitraum = ActivityTime.Älter;
             }
 
             FilterVersammlung = string.Empty;
@@ -162,7 +190,7 @@ namespace Vortragsmanager.ActivityLog
 
         public ObservableCollection<string> ListeFilteredVersammlungen { get; private set; } = new ObservableCollection<string>();
 
-        public ObservableCollection<Item> Alle { get; private set; } = new ObservableCollection<Item>();
+        private List<Item> alleUnfiltered = new List<Item>();
 
         public ObservableCollection<Item> Heute { get; private set; } = new ObservableCollection<Item>();
 
@@ -189,8 +217,9 @@ namespace Vortragsmanager.ActivityLog
             DataContainer.Aktivitäten.Add(message);
 
             var item = new Item(message);
+            item.Zeitraum = ActivityTime.Heute;
             Heute.Insert(0, item);
-            Alle.Add(item);
+            alleUnfiltered.Add(item);
             RaisePropertyChanged(nameof(HeuteHeader));
         }
 
@@ -198,5 +227,15 @@ namespace Vortragsmanager.ActivityLog
         {
             Initialize();
         }
+    }
+
+    public enum ActivityTime
+    {
+        Heute,
+        DieseWoche,
+        DieserMonat,
+        LetzterMonat,
+        DiesesJahr,
+        Älter
     }
 }
