4b9275d01a8bf836c6040603798afcccccad2623
Thomas Ramm <thomas.ramm@gmx.de>
2022-08-03 12:25:59 +0200

    individueller Panungszeitraum für eigene Versammlung

--- Vortragsmanager/Properties/Settings.Designer.cs
+++ Vortragsmanager/Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace Vortragsmanager.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.1.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.2.0.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -293,6 +293,18 @@ namespace Vortragsmanager.Properties {
             }
             set {
                 this["FotoExport"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("12")]
+        public int RednerSuchenAnzahlMonate {
+            get {
+                return ((int)(this["RednerSuchenAnzahlMonate"]));
+            }
+            set {
+                this["RednerSuchenAnzahlMonate"] = value;
             }
         }
     }
